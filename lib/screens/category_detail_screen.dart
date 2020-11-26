d5bf77d4805ef3efda68dd623a0f231a762af8a1
Sushant Ratnam <sushant.ratnam@bhnetwork.com>
2020-11-26 13:52:14 +0530

    created categories and detail screen with images for each meal

---
+++ lib/screens/category_detail_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import '../widgets/meal_item.dart';
+import '../dummy_data.dart';
+
+class CategoryDetail extends StatelessWidget {
+  static const routeName = '/category-meals';
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryDetail(this.categoryId, this.categoryTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryId = routeArgs['id'];
+    final categoryTitle = routeArgs['title'];
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: Center(
+        child: ListView.builder(
+          itemCount: categoryMeals.length,
+          itemBuilder: (context, index) {
+            return MealItem(
+              title: categoryMeals[index].title,
+              imageUrl: categoryMeals[index].imageUrl,
+              duration: categoryMeals[index].duration,
+              complexity: categoryMeals[index].complexity,
+              affordability: categoryMeals[index].affordability,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
